9d3f8e8c73254188e02e26c2302e21495aa135b0
Erianne Viray <erialv@uw.edu>
2017-01-24 14:58:28 -0800

    Add ex2 answers

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -20,27 +20,31 @@ library(fueleconomy)
 View(vehicles)
 
 # Select the different manufacturers (makes) of the cars in this data set.
-
+makes <- select(vehicles, make)
 
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set.
-
+length(unique(makes))
 
 # Filter the data set for vehicles manufactured in 1997
-
+man.1997 <- filter(vehicles, year == 1997)
+View(man.1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function similar to how you would use the `max()` function.
 # See also: https://www.r-bloggers.com/r-sorting-a-data-frame-by-the-contents-of-a-column/
-
+arrange.1997 <- arrange(man.1997, hwy)
+View(arrange.1997)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average gas milage between
 # city and highway for each car
-
+average.1997 <- mutate(arrange.1997, average = (hwy + cty)/2)
+View(average.1997)
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
 # Save this new data frame in a variable.
-
+two.wheel <- filter(vehicles, drive == '2-Wheel Drive', cty > 20)
+View(two.wheel)
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
 # Hint: filter for the worst vecle, then select its ID.
@@ -49,6 +53,12 @@ View(vehicles)
 # Write a function that takes a `year` and a `make` as parameters,
 # and returns the vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
 # You'll need to filter more!
-
+MakeYearFilter <- function(make, year) {
+  filtered <- filter(vehicles, make == make, year == year)
+  filtered <- filter(filtered, hwy == max(hwy))
+  return(filtered)
+}
 
 # What was the most efficient honda model of 1995?
+
+MakeYearFilter('Honda', 1995)
